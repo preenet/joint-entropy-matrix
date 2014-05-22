5745fa4ae55620ed468eafd748ee7dfbcbbba4d2
Pree T. <preenet@users.noreply.github.com>
2014-05-22 18:49:45 +0000

    init

---
+++ JointEntropyMatrix/src/JointEntropyMatrix.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author pthiengburanathum
+ *
+ */
+
+import java.util.Vector;
+
+
+public class JointEntropyMatrix {
+	private Vector<Integer> X;
+	private Vector<Integer> Y;
+	
+	public JointEntropyMatrix(Vector<Integer> X, Vector<Integer> Y) {
+		
+	}// end default constructor
+
+}// end class JointEntropyMatrix
