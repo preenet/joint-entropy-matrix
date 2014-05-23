cd893f641fa8b85289cee0a571536d4ad3e3480c
preenet <preenet@b5040866-ec43-42de-b72f-a4dda858dd52>
2014-05-23 20:43:22 +0000

    added joint mass function class.

---
+++ JointEntropyMatrix/src/JMF.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ * @author Pree
+ *
+ */
+public class JMF {
+	private int x;
+	private int y;
+	private int frequency;
+	private double probability;
+	
+	public JMF() {
+		
+	}
+	
+	public JMF(int x, int y) {
+		
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	/**
+	 * @return the frequency
+	 */
+	public int getFrequency() {
+		return frequency;
+	}
+
+	/**
+	 * @param frequency the frequency to set
+	 */
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+
+	/**
+	 * @return the probability
+	 */
+	public double getProbability() {
+		return probability;
+	}
+
+	/**
+	 * @param probability the probability to set
+	 */
+	public void setProbability(double probability) {
+		this.probability = probability;
+	}
+	
+	
+}// end class
